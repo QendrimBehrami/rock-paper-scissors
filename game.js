2c4af30a40a28df3fa430f0e03fd81c89eee60b8
Qendrim Behrami <behrami@in.tum.de>
2024-02-06 01:21:39 +0100

    Add playRound function

--- game.js
+++ game.js
@@ -2,12 +2,36 @@ function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     case 2:
-      return "Scissors";
+      return "scissors";
     default:
       return null;
+  }
+}
+
+/**
+ * Play one round of Rock-Paper-Scissors
+ * @param {string} playerSelection
+ * @param {string} computerSelection
+ */
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    console.log(`It is a tie! You both chose ${playerSelection}`);
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    console.log(
+      `You have lost! You chose ${playerSelection} and the computer chose ${computerSelection}`
+    );
+  } else {
+    console.log(
+      `You have won! You chose ${playerSelection} and the computer chose ${computerSelection}`
+    );
   }
 }
